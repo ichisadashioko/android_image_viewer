98b94d5656dcea6f5c8ecf5df9410fcad5e0407f
shioko <ichisadashioko@gmail.com>
2023-05-30 07:27:29 +0000

    improve github actions artifact name

--- move_apk_file.py
+++ move_apk_file.py
@@ -14,11 +14,13 @@ input_filepath = 'app/build/outputs/apk/debug/app-debug.apk'
 if not os.path.exists(input_filepath):
     raise Exception(f'{input_filepath} does not exist')
 
-output_filepath = f'android_image_viewer_{time.time_ns()}.apk'
+artifact_filename = f'android_image_viewer_{time.time_ns()}.apk'
+output_filepath = os.path.abspath(artifact_filename)
 print(f'output_filepath {output_filepath}')
 
 if is_github_actions:
     with open('github_actions_artifact_path.log', mode='wb') as outfile:
+        content = f'GITHUB_ARTIFACT_PATH={output_filepath}\nGITHUB_ARTIFACT_NAME={artifact_filename}'
         outfile.write(output_filepath.encode('utf-8'))
 
 os.rename(input_filepath, output_filepath)
